4b609163180311f8dad9fd65b92115149d0c9d6c
arnaud ferrand <arferrand@gmail.com>
2017-07-14 18:46:52 +0100

    adding autosuggest-standardblend-grid

---
+++ test/what3words/config.spec.js
@@ -0,0 +1,54 @@
+const what3words = require('../../');
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+const expect = require('chai').expect;
+const config = require('../../lib/what3words/config.js');
+
+const ERRORS = what3words.errors;
+
+/* eslint no-console: "off" */
+
+/* eslint no-unused-expressions: "off" */
+
+describe('#config ', () => {
+  const apiKey = process.env.W3W_API_KEY;
+  it('getApiKey() equal env W3W_API_KEY', () => {
+    const key = config.getApiKey();
+    expect(apiKey).to.exist;
+    expect(key).to.exist;
+    expect(apiKey).to.equal(key);
+  });
+  it('endpoint to equal https://api.what3words.com/v2', () => {
+    const ENDPOINT = 'https://api.what3words.com/v2';
+    expect(config.getEndpoint()).to.equal(ENDPOINT);
+  });
+  describe('endpoint env empty', () => {
+    before(() => {
+      process.env.W3W_API_HOST = '';
+    });
+    it('throws an error', () => {
+      const fn = () => {
+        config.getEndpoint();
+      };
+      expect(fn).to.throw;
+      expect(fn).to.throw(ERRORS.INVALID_ENDPOINT);
+    });
+    after(() => {
+      delete process.env.W3W_API_HOST;
+    });
+  });
+  describe('endpoint env invalid', () => {
+    before(() => {
+      process.env.W3W_API_HOST = '123';
+    });
+    it('endpoint numeric value', () => {
+      const fn = () => {
+        config.getEndpoint();
+      };
+      expect(fn).to.throw;
+      expect(fn).to.throw(ERRORS.INVALID_ENDPOINT);
+    });
+    after(() => {
+      delete process.env.W3W_API_HOST;
+    });
+  });
+});
---
+++ test/what3words/standardblend.spec.js
@@ -0,0 +1,160 @@
+const what3words = require('../../');
+const validateAS = require('./validateAS');
+
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+const expect = require('chai').expect;
+
+const ERRORS = what3words.errors;
+
+/* eslint no-console: "off" */
+
+/* eslint no-unused-expressions: "off" */
+
+describe('#standardblend ', () => {
+  describe('input params', () => {
+    it('expects to throw with no params', () => {
+      const fn = () => {
+        what3words.standardblend();
+      };
+      expect(fn).to.throw;
+      expect(fn).to.throw(ERRORS.UNDEFINED_QUERY);
+    });
+    it('expects to throw with null', () => {
+      const fn = () => {
+        what3words.standardblend(null);
+      };
+      expect(fn).to.throw;
+      expect(fn).to.throw(ERRORS.UNDEFINED_QUERY);
+    });
+  });
+
+  describe('fails', () => {
+    it('expects to fail with no addr', (done) => {
+      what3words
+        .standardblend({})
+        .then(
+          (resolved) => {
+            // console.log(resolved);
+            const data = JSON.parse(resolved);
+            expect(data).to.exist;
+            expect(data.code).to.exist;
+            expect(data.code).to.equal(400);
+            expect(data.message).to.exist;
+            expect(data.message).to.equal('/standardblend: missing required parameter "addr"');
+            done();
+          },
+          (rejected) => {
+            console.log(rejected);
+            done(rejected);
+          })
+        .catch((err) => {
+          done(err);
+        });
+    });
+
+    it('expects to fail with no lang', (done) => {
+      what3words
+        .standardblend({
+          addr: 'abc',
+        })
+        .then(
+          (resolved) => {
+            // console.log(resolved);
+            const data = JSON.parse(resolved);
+            expect(data).to.exist;
+            expect(data.code).to.exist;
+            expect(data.code).to.equal(400);
+            expect(data.message).to.exist;
+            expect(data.message).to.equal('/standardblend: missing required parameter "lang"');
+            done();
+          },
+          (rejected) => {
+            console.log(rejected);
+            done(rejected);
+          })
+        .catch((err) => {
+          done(err);
+        });
+    });
+
+    xit('expects to fail with invalid addr', (done) => {
+      what3words
+        .standardblend({
+          addr: 'abc',
+          lang: 'en',
+        })
+        .then(
+          (resolved) => {
+            // console.log(resolved);
+            const data = JSON.parse(resolved);
+            expect(data).to.exist;
+            expect(data.status).to.exist;
+            expect(data.status.status).to.exist;
+            expect(data.status.status).to.equal(200);
+            expect(data.status.code).to.exist;
+            expect(data.status.code).to.equal(102);
+            expect(data.status.message).to.exist;
+            expect(data.status.message).to.equal('The \'addr\' parameter is invalid or missing a partial or complete 3 word address');
+            done();
+          },
+          (rejected) => {
+            console.log(rejected);
+            done(rejected);
+          })
+        .catch((err) => {
+          done(err);
+        });
+    });
+  });
+
+  xdescribe('success', () => {
+    const addr = 'index.home.raft';
+
+    it(`should forward [${addr}] in \`json\``, (done) => {
+      const params = {
+        addr,
+        format: 'json',
+      };
+      what3words
+        .forward(params)
+        .then(
+          (resolved) => {
+            // console.log(resolved);
+            validateAS.validateJSONPayload(JSON.parse(resolved));
+            done();
+          },
+          (rejected) => {
+            console.log('rejected', rejected);
+            done(rejected);
+          })
+        .catch((err) => {
+          console.log('catch', err);
+          done(err);
+        });
+    });
+
+    it(`should forward [${addr}] in \`geojson\``, (done) => {
+      const params = {
+        addr,
+        format: 'geojson',
+      };
+      what3words
+        .forward(params)
+        .then(
+          (resolved) => {
+            // console.log(resolved);
+            validateAS.validateGeoJSONPayload(JSON.parse(resolved));
+            done();
+          },
+          (rejected) => {
+            console.log('rejected', rejected);
+            done(rejected);
+          })
+        .catch((err) => {
+          console.log('catch', err);
+          done(err);
+        });
+    });
+    // end of geojson
+  });
+});
