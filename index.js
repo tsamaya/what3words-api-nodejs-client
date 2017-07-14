ae4d77ffa3fe4d1ba72f0d564508087119db7690
arnaud ferrand <arferrand@gmail.com>
2017-07-14 18:43:48 +0100

    adding autosuggest-standardblend-grid

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const languages = require('./lib/what3words/languages');
 const reverse = require('./lib/what3words/reverse');
 const forward = require('./lib/what3words/forward');
+const autosuggest = require('./lib/what3words/autosuggest');
+const standardblend = require('./lib/what3words/standardblend');
+const grid = require('./lib/what3words/grid');
 const errors = require('./lib/what3words/errors');
 
 /**
@@ -11,5 +14,8 @@ module.exports = {
   languages,
   reverse,
   forward,
+  autosuggest,
+  standardblend,
+  grid,
   errors,
 };
